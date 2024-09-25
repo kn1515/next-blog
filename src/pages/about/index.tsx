ac7ea1497dbdad12a54d04aa58a6b8a37b0a3893
kn1515 <daikonha117@gmail.com>
2024-09-26 02:03:40 +0900

    bio modified

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -4,6 +4,7 @@ import Header from "../../components/header";
 import Head from "next/head";
 import { CMS_NAME } from "../../lib/constants";
 import { FaPersonWalking, FaCube, FaChartLine } from "react-icons/fa6";
+import { DiCode } from "react-icons/di";
 import { IconContext } from "react-icons";
 
 export default function About() {
@@ -17,101 +18,71 @@ export default function About() {
         <Container>
           <Header />
 
-          <section className="body-font">
-            <div className="lg:w-4/6 mx-auto">
-              <div className="flex flex-col sm:flex-row mt-10">
-                <div className="sm:w-1/3 text-start sm:pr-8 sm:py-8">
-                  <div className="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200">
-                    <IconContext.Provider value={{ size: "4em" }}>
-                      <FaPersonWalking />
-                    </IconContext.Provider>
-                  </div>
-                  <div className="flex flex-col items-left text-left justify-left">
-                    <h2 className="font-medium title-font mt-4 text-lg">
-                      Who are you ...?
-                    </h2>
-                    <div className="w-12 h-1 bg-indigo-500 rounded mt-2 mb-4"></div>
-                  </div>
+          <>
+            <Head>
+              <title>About</title>
+            </Head>
+            <div className="container mx-auto px-4 py-8 flex items-center justify-center min-h-screen">
+              <div className="w-full max-w-4xl text-center">
+                <h1 className="text-4xl font-bold mb-4">About</h1>
+                <div className="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 mx-auto">
+                  <IconContext.Provider value={{ size: "4em" }}>
+                    <FaPersonWalking />
+                  </IconContext.Provider>
                 </div>
-                <div className="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-right">
-                  <p className="leading-relaxed text-xg m-8">
-                    「ぽん」と申します。
-                    <br />
-                    Sierでバックエンドエンジニアとして働いています。
-                    <br />
-                    主にPHP/LaravelでのAPI開発を行っています。
-                    <br />
-                    <br />
-                  </p>
+                <p className="text-lg mb-4">
+                  ぽんと申します。
+                  <br />
+                  Sierでバックエンドエンジニアとして働いています。
+                  <br />
+                  主にPHP/LaravelでのAPI開発を行っています。
+                </p>
+                <div className="w-12 h-1 bg-indigo-500 rounded mt-4 mb-8 mx-auto">
+                  {" "}
                 </div>
-              </div>
-            </div>
-          </section>
-
-          <section className="body-font">
-            <div className="lg:w-4/6 mx-auto">
-              <div className="flex flex-col sm:flex-row mt-10">
-                <div className="sm:w-1/3 text-center sm:pr-8 sm:py-8">
-                  <div className="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200">
-                    <IconContext.Provider value={{ size: "4em" }}>
-                      <FaCube />
-                    </IconContext.Provider>
-                  </div>
-                  <div className="flex flex-col items-center text-center justify-center">
-                    <h2 className="font-medium title-font mt-4 text-lg">
-                      Skill
-                    </h2>
-                    <div className="w-12 h-1 bg-indigo-500 rounded mt-2 mb-4">
-                      {"   "}
-                    </div>
-                  </div>
+                <h2 className="text-3xl font-semibold mb-3 mt-6">Skills</h2>
+                <div className="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 mx-auto">
+                  <IconContext.Provider value={{ size: "4em" }}>
+                    <FaCube />
+                  </IconContext.Provider>
                 </div>
-                <div className="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
-                  <p className="leading-relaxed text-lg mb-4">
-                    Backend : PHP, Perl, Python, PostgreSQL
-                    <br />
-                    Frontend : React, TypeScript
-                    <br />
-                  </p>
+                <h3 className="text-3sm font-semibold mb-3 mt-6">Backend</h3>
+                <ul className="list-disc list-inside mb-4">
+                  <li>PHP/Laravel</li>
+                  <li>Python</li>
+                  <li>Java</li>
+                  <li>Docker</li>
+                  <li>PostgreSQL</li>
+                </ul>
+                <div className="w-12 h-1 bg-indigo-500 rounded mt-4 mb-8 mx-auto">
+                  {" "}
                 </div>
-              </div>
-            </div>
-          </section>
-
-          <section className="body-font">
-            <div className="lg:w-4/6 mx-auto">
-              <div className="flex flex-col sm:flex-row mt-10">
-                <div className="sm:w-1/3 text-center sm:pr-8 sm:py-8">
-                  <div className="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200">
-                    <IconContext.Provider value={{ size: "4em" }}>
-                      <FaChartLine />
-                    </IconContext.Provider>
-                  </div>
-                  <div className="flex flex-col items-center text-center justify-center">
-                    <h2 className="font-medium title-font mt-4 text-lg">
-                      Discography
-                    </h2>
-                    <div className="w-12 h-1 bg-indigo-500 rounded mt-2 mb-4"></div>
-                  </div>
+                <div className="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 mx-auto">
+                  <IconContext.Provider value={{ size: "4em" }}>
+                    <DiCode />
+                  </IconContext.Provider>
                 </div>
-                <div className="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
-                  <h2 className="font-medium title-font mt-4 text-base">
-                    2022 ~ now
-                  </h2>
-                  <p className="mb-4 mt-1">XXX</p>
-                  <h2 className="font-medium title-font mt-4 text-base">
-                    2020 ~ 2022
-                  </h2>
-                  <p className="mb-4 mt-1">株式会社XXX</p>
-                  <h2 className="font-medium title-font mt-4 text-base">
-                    2016 ~ 2020
-                  </h2>
-                  <p className="mb-4 mt-1">Hoge大学　Hoge学部</p>
-                  <p className="leading-relaxed text-lg mb-4"> </p>
+                <h3 className="text-3sm font-semibold mb-3 mt-6">FrontEnd</h3>
+                <ul className="list-disc list-inside mb-4">
+                  <li>React.js</li>
+                  <li>JavaScript/TypeScript</li>
+                  <li>tailwindcss</li>
+                  <li>Material UI</li>
+                </ul>
+                <div className="w-12 h-1 bg-indigo-500 rounded mt-4 mb-8 mx-auto">
+                  {" "}
+                </div>
+                <pre className="bg-gray-100 p-4 rounded mb-4 mx-auto">
+                  <code>
+                    {`const greeting = "Hello, world!";\nconsole.log(greeting);`}
+                  </code>
+                </pre>
+                <div className="w-12 h-1 bg-indigo-500 rounded mt-4 mb-8 mx-auto">
+                  {" "}
                 </div>
               </div>
             </div>
-          </section>
+          </>
         </Container>
       </Layout>
     </>
