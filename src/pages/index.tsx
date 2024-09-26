40f2aa7047f7243116d70439f662c92cbc0708c2
kn1515 <daikonha117@gmail.com>
2024-09-27 02:30:52 +0900

    TOPに紹介画像を追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,6 +26,11 @@ const Home: NextPage<Props> = ({ allPosts }) => {
       {/* Hero Section */}
       <div className="bg-indigo-600 text-white py-24 sm:py-32">
         <div className="container mx-auto px-6 lg:px-8 text-center">
+          <img
+            src="https://github.com/kn1515.png"
+            alt="GitHub Profile"
+            className="w-32 h-32 rounded-full mx-auto mb-4"
+          />
           <h1 className="text-5xl font-bold mb-4">ぽんろぐ</h1>
           <p className="text-lg mb-8">
             あるエンジニアのIT技術関連に関する備忘録
