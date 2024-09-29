382cb6875909ab2c0be0f4329bd50d7569b23764
kn1515 <daikonha117@gmail.com>
2024-09-29 15:06:18 +0900

    タイトル変更

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const HeroSection = () => {
   return (
     <div className="bg-indigo-600 text-white py-24 sm:py-32">
@@ -9,7 +7,7 @@ const HeroSection = () => {
           alt="GitHub Profile"
           className="w-32 h-32 rounded-full mx-auto mb-4"
         />
-        <h1 className="text-5xl font-bold mb-4">ぽんろぐ</h1>
+        <h1 className="text-5xl font-bold mb-4">ぽんろぐ備忘録</h1>
         <p className="text-lg mb-8">あるエンジニアのIT技術関連に関する備忘録</p>
         <a
           href="#posts"
